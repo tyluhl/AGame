a055ea6f4a3245485f0ae83b2e37597d2738677c
ansmor <ansmor21@bergen.org>
2018-04-11 09:51:30 -0400

    zombie objects

--- EnvironmentObject.js
+++ EnvironmentObject.js
@@ -1,15 +1,10 @@
 class environment{
 
-    constructor(startPoint, endPoint, width, height){
-        this.startPoint = startPoint;
-        this.endPoint = endPoint;
+    constructor(startPoint, startyPoint, width, height){
+        this.startxPoint = startPoint;
+        this.startyPoint = endPoint;
         this.width = width;
         this.height = height;
-        ctx.beginPath();
-        ctx.rect(startPoint, endPoint, width, height);
-        ctx.fillStyle = "#000000";
-        ctx.fill();
-        ctx.closePath();
     }
 
 
