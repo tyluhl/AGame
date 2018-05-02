13bce771c20ba2d5ebca532798b63cc189adaf97
ArjunNair03 <arjun@nairsnet.com>
2018-05-02 09:23:16 -0400

    revert back to working version

--- EnvironmentObject.js
+++
@@ -1,7 +0,0 @@
-
-function Environment(startx, starty, width, height){
-        this.startx = startx;
-        this.starty = starty;
-        this.width = width;
-        this.height = height;
-    }
